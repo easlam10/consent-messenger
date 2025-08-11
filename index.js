11251a4e8a6d44067cd686db7a7a541416b394a4
Ehtisham Aslam <ehtisham.ea10@gmail.com>
2025-08-11 21:30:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const sendTemplateMessage = async () => {
+  const phoneNumberId = '704538612744489'; // From Meta dashboard
+  const toNumber = process.env.RECIEVER_NUMBER; // Recipient number
+  const accessToken = 'EAATrndy9e08BPI5tgY3Qs9Hk87u78Ula93Pwu2RAZBPiEwwmBj2mfZAcjbhJrcgbkdmUnLRBmvorgUR34hgcDykQZBwcBZB61lOBZBTDTHZBqw1V6l92qv9BcmHRHQSm3CsgL19ag7wseb0se6teMWdi6gPY30ZCaIZAYtlmIWZCeuENb0BvwJ1Qy3S0FJFa7FTpkVAZDZD'; // System user access token
+
+  try {
+    const response = await axios.post(
+      `https://graph.facebook.com/v19.0/${phoneNumberId}/messages`,
+      {
+        messaging_product: "whatsapp",
+        to: toNumber,
+        type: "template",
+        template: {
+          name: "kips_alerts",
+          language: {
+            code: "en_US"
+          }
+        }
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    console.log('Message sent successfully:', response.data);
+  } catch (error) {
+    console.error('Error sending message:', error.response?.data || error.message);
+  }
+};
+
+sendTemplateMessage();
